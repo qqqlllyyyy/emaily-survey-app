263bff46ac04bd73ee3690b51b9147ae138f8ded
qqqlllyyyy <qinliyu1990@gmail.com>
2017-09-01 02:22:45 -0700

    Finished auth flow

--- config/keys.js
+++ config/keys.js
@@ -2,6 +2,7 @@
 // This is injected by Heroku
 if (process.env.NODE_ENV === "production") {
   // We are in production
+  module.exports = require("./prod");
 } else {
   // We are in dev env
   module.exports = require("./dev");
