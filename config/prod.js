fc451ce3f828bf5c3702922f62ce7e631805eacf
qqqlllyyyy <qinliyu1990@gmail.com>
2017-09-01 02:20:55 -0700

    Finished auth flow

--- config/prod.js
+++ config/prod.js
@@ -5,8 +5,3 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
 };
-
-// New Google Credentials
-// 639714899373-rkfm2o2ut8h7bp33s1qllgjdt6fttrdp.apps.googleusercontent.com
-// d-_SzLHZXUGT9lrtEUb8x69C
-// mongodb://qqqlllyyyy:90109010q@ds115214.mlab.com:15214/emaily-qinliyu
