3747c9ec79fbf23df6d27ac59b77a2bbaf2c1baf
qqqlllyyyy <qinliyu1990@gmail.com>
2017-09-07 20:48:22 -0700

    React setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import React from "react";
+import ReactDOM from "react-dom";
+/**
+ * @param Root component
+ * @param Where we're going to render the component to inside our dom
+ */
+ReactDom.render();
